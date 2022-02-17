fecba707470696b9be2144d9675c48d411a2eb48
David Malpica <dmalpica96@hotmail.com>
2022-02-17 17:35:54 -0600

    Initial commit

---
+++ Modulo10/config.py
@@ -0,0 +1,14 @@
+# def main():
+#     try:
+#         configuration = open('config.txt')
+#     except FileNotFoundError:
+#         print("Couldn't find the config.txt file!")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")
+
+if __name__ == '__main__':
+    main()
